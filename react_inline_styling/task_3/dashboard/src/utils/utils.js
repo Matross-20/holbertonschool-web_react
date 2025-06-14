9c9ea1f1d8e0f206c8efb86ae99fd5b5e845c052
Matross-20 <matrossmthawelanga@gmail.com>
2025-06-14 18:34:05 +0200

    React Inline Styling

--- react_inline_styling/task_3/dashboard/src/utils/utils.js
+++ react_inline_styling/task_3/dashboard/src/utils/utils.js
@@ -1,6 +1,11 @@
-export const getFullYear = () => { return new Date().getFullYear() }
-export const getFooterCopy = (isIndex) => {
-	if (isIndex) return 'Holberton School'
-	return 'Holberton School main dashboard'
-}
-export const getLatestNotification = () => { return '<strong>Urgent requirement</strong> - complete by EOD' }
+export function getCurrentYear() {
+    return new Date().getFullYear();
+  }
+
+  export function getFooterCopy(isIndex) {
+    return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+  }
+
+  export function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+  }
