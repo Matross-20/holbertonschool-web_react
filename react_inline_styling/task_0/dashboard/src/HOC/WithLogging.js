7041ef6e11b317679f0ee5476abf112c9f4566ce
Matross-20 <matrossmthawelanga@gmail.com>
2025-06-22 05:07:57 +0200

    React Inline Style

---
+++ react_inline_styling/task_0/dashboard/src/HOC/WithLogging.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+function WithLogging(WrappedComponent) {
+  return class extends Component {
+    static displayName = `WithLogging(${WrappedComponent.displayName || WrappedComponent.name || 'Component'
+      })`
+
+    componentDidMount() {
+      const componentName =
+        WrappedComponent.displayName || WrappedComponent.name || 'Component'
+      console.log(`Component ${componentName} is mounted`)
+    }
+
+    componentWillUnmount() {
+      const componentName =
+        WrappedComponent.displayName || WrappedComponent.name || 'Component'
+      console.log(`Component ${componentName} is going to unmount`)
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+}
+
+export default WithLogging
